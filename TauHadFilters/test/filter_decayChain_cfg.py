70cac142da1ca43084ed70b8f33968b90714e526
Brandon Chiarito <bchiarito@gmail.com>
2018-03-29 14:43:05 -0400

    new cfgs

--- TauHadFilters/test/filter_decayChain_cfg.py
+++ TauHadFilters/test/filter_decayChain_cfg.py
@@ -5,8 +5,8 @@ process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
   #'file:/cms/chiarito/samples/ztoll/miniaod/merged10k_30prong_pt40_eta25_MINIAOD.root'
@@ -14,7 +14,6 @@ process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
   #'/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/100000/00099D43-77ED-E611-8889-5065F381E1A1.root' # 50+
 
   ))
-process.TFileService = cms.Service("TFileService", fileName = cms.string( "output_filter_tree_10-50.root" ) )
 
 process.filt = cms.EDFilter('ZtoTauHadTruthSelector',
   filterByTruthDecayType = cms.untracked.vdouble(0.0),
@@ -22,7 +21,15 @@ process.filt = cms.EDFilter('ZtoTauHadTruthSelector',
   #absEtaMax = cms.untracked.double(2.5), 
   )
 
-process.tree = cms.EDAnalyzer('ZtoTauHadTreeMaker',
-  )
+# Draws gen particle decay chain
+process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
+                                   src = cms.InputTag("prunedGenParticles"),
+                                   printStatus = cms.untracked.bool(True),
+                                   printP4 = cms.untracked.bool(False),
+                                   printPtEtaPhi = cms.untracked.bool(False),
+                                   printVertex = cms.untracked.bool(False),
+                                   printIndex = cms.untracked.bool(False),
+                                   #status = cms.untracked.vint32( 3 )
+)
 
-process.p = cms.Path(process.filt * process.tree)
+process.p = cms.Path(process.filt * process.printTree)
--- TauHadFilters/test/genDecayChain_cfg.py
+++ TauHadFilters/test/genDecayChain_cfg.py
@@ -4,13 +4,14 @@ process = cms.Process("GENANA")
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(3) )
 process.source = cms.Source("PoolSource",
         fileNames = cms.untracked.vstring(
-        #'/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2/120000/02A210D6-F5C3-E611-B570-008CFA197BD4.root'
+        #'/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/00271851-3CC8-E611-AFB9-002590D0AFD8.root' # 10-50
+        '/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/100000/00099D43-77ED-E611-8889-5065F381E1A1.root' # 50+
 )
 )
 
@@ -32,4 +33,4 @@ process.printTree = cms.EDAnalyzer("ParticleTreeDrawer",
                                    #status = cms.untracked.vint32( 3 )
 )
 
-process.path = cms.Path(process.printList * process.printTree)
+process.path = cms.Path(process.printTree)
