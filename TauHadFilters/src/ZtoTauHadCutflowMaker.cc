9c8d4755dc5d83bd177cfc1f2ae8381e90d695b9
Brandon Chiarito <bchiarito@gmail.com>
2018-05-03 12:59:51 -0400

    new cutlfow maker analyzer

---
+++ TauHadFilters/src/ZtoTauHadCutflowMaker.cc
@@ -0,0 +1,510 @@
+// -*- C++ -*-
+//
+// Package:    TauHadFilters/ZtoTauHadCutflowMaker
+// Class:      ZtoTauHadCutflowMaker
+// 
+/**\class ZtoTauHadCutflowMaker ZtoTauHadCutflowMaker.cc TauHadFilters/ZtoTauHadCutflowMaker/plugins/ZtoTauHadCutflowMaker.cc
+
+ Description: [one line class summary]
+
+*/
+//
+// Original Author:  Brandon Chiarito
+//         Created:  Wed, 05 Jul 2017 22:31:58 GMT
+//
+//
+
+// cpp includes
+#include <memory>
+#include <math.h>
+#include <algorithm>
+#include <iostream>
+// ROOT includes
+#include "TVector2.h"
+#include "TLorentzVector.h"
+#include "TH1.h"
+#include "TTree.h"
+// cmssw framework includes
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDFilter.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/StreamID.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+// trigger inlcudes
+#include "DataFormats/Math/interface/deltaR.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h"
+#include "DataFormats/PatCandidates/interface/PackedTriggerPrescales.h"
+// pat includes
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
+#include "DataFormats/PatCandidates/interface/Photon.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h"
+// edm utilities includes
+#include "DataFormats/Math/interface/deltaR.h"
+
+// namesspace defaults
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+//
+// class declaration
+//
+class ZtoTauHadCutflowMaker : public edm::one::EDAnalyzer<edm::one::SharedResources> {
+   public:
+      explicit ZtoTauHadCutflowMaker(const edm::ParameterSet&);
+      ~ZtoTauHadCutflowMaker();
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+   private:
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+
+      // Configuration Parameters
+      bool cfg_tauObjs;
+
+      // EDM Collection lables
+      edm::EDGetTokenT<edm::TriggerResults> triggerBits_;
+      edm::EDGetTokenT<pat::TriggerObjectStandAloneCollection> triggerObjects_;
+      edm::EDGetTokenT<pat::PackedTriggerPrescales> triggerPrescales_;
+      edm::EDGetTokenT<reco::VertexCollection> vtxToken_;
+      edm::EDGetTokenT<pat::MuonCollection> muonToken_;
+      edm::EDGetTokenT<pat::ElectronCollection> electronToken_;
+      edm::EDGetTokenT<pat::TauCollection> tauToken_;
+      edm::EDGetTokenT<pat::PhotonCollection> photonToken_;
+      edm::EDGetTokenT<pat::JetCollection> jetToken_;
+      edm::EDGetTokenT<pat::JetCollection> fatjetToken_;
+      edm::EDGetTokenT<pat::METCollection> metToken_;
+      edm::EDGetTokenT<pat::PackedCandidateCollection> pfcandsToken_;
+      edm::EDGetTokenT<reco::BeamSpot> beamToken_;
+      edm::EDGetTokenT<double> rhoToken_;
+
+      // tree and branches
+      TTree* tree_;
+      // event wide
+      int bevent;
+      int brun;
+      int blumi;
+      // cutflow
+      int bpassMuonTrigger;
+      int bpassMuon;
+      int bpassTauCand;
+      int bpassTauMuonPair;
+      int bpassDR;
+      int bpassMT;
+      int bpassPzeta;
+      int bpassExtraLep;
+      int bpassBTag;
+      int bpassAll;
+};
+
+//
+// constructors
+//
+ZtoTauHadCutflowMaker::ZtoTauHadCutflowMaker(const edm::ParameterSet& iConfig) :
+  cfg_tauObjs(iConfig.getUntrackedParameter<bool>("tauObjs"))
+{
+  triggerBits_ = consumes<edm::TriggerResults>( edm::InputTag("TriggerResults","","HLT") );
+  triggerObjects_ = consumes<pat::TriggerObjectStandAloneCollection>( edm::InputTag("selectedPatTrigger") );
+  triggerPrescales_ = consumes<pat::PackedTriggerPrescales>( edm::InputTag("patTrigger") );
+  vtxToken_ = consumes<vector<reco::Vertex>>(edm::InputTag("offlineSlimmedPrimaryVertices"));
+  muonToken_ = consumes<std::vector<pat::Muon>>(edm::InputTag("slimmedMuons"));
+  electronToken_ = consumes<std::vector<pat::Electron>>(edm::InputTag("slimmedElectrons"));
+  tauToken_ = consumes<std::vector<pat::Tau>>(edm::InputTag("slimmedTaus"));
+  photonToken_ = consumes<std::vector<pat::Photon>>(edm::InputTag("slimmedPhotons"));
+  jetToken_ = consumes<std::vector<pat::Jet>>(edm::InputTag("slimmedJets"));
+  fatjetToken_ = consumes<std::vector<pat::Jet>>(edm::InputTag("slimmedJetsAK8"));
+  metToken_ = consumes<std::vector<pat::MET>>(edm::InputTag("slimmedMETs"));
+  pfcandsToken_ = consumes<pat::PackedCandidateCollection>(edm::InputTag("packedPFCandidates"));
+  beamToken_ = consumes<reco::BeamSpot>(edm::InputTag("offlineBeamSpot"));
+  rhoToken_ = consumes<double>(edm::InputTag("fixedGridRhoFastjetCentralChargedPileUp"));
+
+  edm::Service<TFileService> fs;
+  tree_ = fs->make<TTree>("tree","ZtoTauHadCutflowMakerTree");
+  // event wide
+  tree_->Branch("eventNum",&bevent,"eventNum/I");
+  tree_->Branch("runNum",&brun,"runNum/I");
+  tree_->Branch("lumiNum",&blumi,"lumiNum/I");
+  // cutflow
+  tree_->Branch("passMuonTrigger",&bpassMuonTrigger,"passMuonTrigger/I");
+  tree_->Branch("passMuon",&bpassMuon,"passMuon/I");
+  tree_->Branch("passTauCand",&bpassTauCand,"passTauCand/I");
+  tree_->Branch("passTauMuonPair",&bpassTauMuonPair,"passTauMuonPair/I");
+  tree_->Branch("passDR",&bpassDR,"passDR/I");
+  tree_->Branch("passMT",&bpassMT,"passMT/I");
+  tree_->Branch("passPzeta",&bpassPzeta,"passPzeta/I");
+  tree_->Branch("passExtraLep",&bpassExtraLep,"passExtraLep/I");
+  tree_->Branch("passBTag",&bpassBTag,"passBTag/I");
+  tree_->Branch("passAll",&bpassAll,"passAll/I");
+}
+
+//
+// destructor
+//
+ZtoTauHadCutflowMaker::~ZtoTauHadCutflowMaker()
+{
+}
+
+// ------------ method called on each new Event  ------------
+void
+ZtoTauHadCutflowMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  // get event content
+  edm::Handle<edm::TriggerResults> triggerBits;
+  iEvent.getByToken(triggerBits_, triggerBits);
+
+  edm::Handle<pat::TriggerObjectStandAloneCollection> triggerObjects;
+  iEvent.getByToken(triggerObjects_, triggerObjects);
+
+  edm::Handle<pat::PackedTriggerPrescales> triggerPrescales;
+  iEvent.getByToken(triggerPrescales_, triggerPrescales);
+
+  edm::Handle<reco::VertexCollection> vertices;
+  iEvent.getByToken(vtxToken_, vertices);
+  const reco::Vertex &PV = vertices->front();
+
+  edm::Handle<pat::TauCollection> taus;
+  iEvent.getByToken(tauToken_, taus);
+
+  edm::Handle<pat::MuonCollection> muons;
+  iEvent.getByToken(muonToken_, muons);
+
+  edm::Handle<pat::ElectronCollection> electrons;
+  iEvent.getByToken(electronToken_, electrons);
+ 
+  edm::Handle<pat::JetCollection> jets;
+  iEvent.getByToken(jetToken_, jets);
+
+  edm::Handle<pat::METCollection> mets;
+  iEvent.getByToken(metToken_, mets);
+  pat::MET MET = (*mets)[0];
+
+  edm::Handle<double> rho;
+  iEvent.getByToken(rhoToken_, rho);
+
+  // initialize branches
+  bevent = iEvent.id().event();
+  brun = iEvent.id().run();
+  blumi = iEvent.id().luminosityBlock();
+
+  bpassMuonTrigger = false;
+  bpassMuon = false;
+  bpassTauCand = false;
+  bpassTauMuonPair = false;
+  bpassDR = false;
+  bpassMT = false;
+  bpassPzeta = false;
+  bpassExtraLep = false;
+  bpassBTag = false;
+  bpassAll = false;
+
+  // debug: verify both accessors for muon tag are equivilant
+  for (const pat::Muon &muon : *muons) {
+    if ( muon.isGlobalMuon() != muon.muonID("AllGlobalMuons") )
+      std::cout << "disagree global: " << muon.isGlobalMuon() << ", " << muon.muonID("AllGlobalMuons") << std::endl;
+    if ( muon.isTrackerMuon() != muon.muonID("AllTrackerMuons") )
+      std::cout << "diagree tracker: " << muon.isTrackerMuon() << ", " << muon.muonID("AllTrackerMuons") << std::endl; 
+  }
+
+  // trigger 
+  const edm::TriggerNames &names = iEvent.triggerNames(*triggerBits);
+  string trigger_muon = "HLT_IsoMu20";
+  bool found_muon = false;
+  bool bit_muon = false;
+  string name_muon = "";
+  for (unsigned int i = 0, n = triggerBits->size(); i < n; i++)
+  {
+     string triggerName = names.triggerName(i);
+
+     std::size_t pos = triggerName.find(trigger_muon);
+     if ( pos != std::string::npos ) {
+       found_muon = true;
+       bit_muon = triggerBits->accept(i);
+       name_muon = triggerName;
+     }
+  }
+  if (!found_muon) std::cout << "failed to find muon trigger!" << std::endl;
+  bool passMuonTrigger = bit_muon;
+
+  // muons
+  vector<const pat::Muon *> passedMuons;
+  for (const pat::Muon &muon : *muons) {
+    if (muon.pt() > 21.0 &&
+        fabs(muon.eta()) < 2.1 &&
+        //(muon.chargedHadronIso() + muon.neutralHadronIso() + muon.photonIso())/muon.pt() - 0.5 * (*rho) < 0.1 &&
+        (muon.chargedHadronIso() + muon.neutralHadronIso() + muon.photonIso())/muon.pt() < 0.1 &&
+        muon.muonBestTrack()->dz(PV.position()) < 0.2 &&
+        abs(muon.muonBestTrack()->dxy(PV.position())) < 0.045 &&
+        muon.isMediumMuon() )
+      passedMuons.push_back(&muon);
+  }
+
+  // extra lepton veto
+  bool skipped_one_passing_muon = false;
+  bool extraMuon = false;
+  for (const pat::Muon &muon : *muons) {
+    if (muon.pt() > 10.0 &&
+        fabs(muon.eta()) < 2.4 &&
+        //(muon.chargedHadronIso() + muon.neutralHadronIso() + muon.photonIso())/muon.pt() - 0.5 * (*rho) < 0.3 &&
+        (muon.chargedHadronIso() + muon.neutralHadronIso() + muon.photonIso())/muon.pt() < 0.3 &&
+        muon.muonBestTrack()->dz(PV.position()) < 0.2 &&
+        abs(muon.muonBestTrack()->dxy(PV.position())) < 0.045 &&
+        muon.isMediumMuon() ) {
+      if (muon.pt() > 19.0 &&
+          fabs(muon.eta()) < 2.1 &&
+          //(muon.chargedHadronIso() + muon.neutralHadronIso() + muon.photonIso())/muon.pt() - 0.5 * (*rho) < 0.1 &&
+          (muon.chargedHadronIso() + muon.neutralHadronIso() + muon.photonIso())/muon.pt() < 0.1 &&
+          muon.muonBestTrack()->dz(PV.position()) < 0.2 &&
+          abs(muon.muonBestTrack()->dxy(PV.position())) < 0.045 &&
+          muon.isMediumMuon() ) {
+        if (skipped_one_passing_muon) extraMuon = true;
+        if (!skipped_one_passing_muon) skipped_one_passing_muon = true;
+      } else {
+        extraMuon = true;
+      }
+    }
+  }
+  bool extraElectron = false;
+  for (const pat::Electron &electron : *electrons) {
+    if (electron.pt() > 10.0 &&
+        fabs(electron.eta()) < 2.5 &&
+        electron.gsfTrack()->dz(PV.position()) < 0.2 &&
+        abs(electron.gsfTrack()->dxy(PV.position())) < 0.045 &&
+        electron.passConversionVeto() &&
+        electron.gsfTrack()->lost() <= 1)
+      extraElectron = true;
+  }
+  bool diMuon = false;
+  for (const pat::Muon &muon1 : *muons) {
+    for (const pat::Muon &muon2 : *muons) {
+      double dR = deltaR(muon1.eta(), muon1.phi(), muon2.eta(), muon2.phi());
+      if (dR <= 0.15) continue;
+      if (muon1.pt() > 15 &&
+          fabs(muon1.eta()) < 2.4 &&
+          //(muon1.chargedHadronIso() + muon1.neutralHadronIso() + muon1.photonIso())/muon1.pt() - 0.5 * (*rho) < 0.3 &&
+          (muon1.chargedHadronIso() + muon1.neutralHadronIso() + muon1.photonIso())/muon1.pt() < 0.3 &&
+          muon1.muonBestTrack()->dz(PV.position()) < 0.2 &&
+          abs(muon1.muonBestTrack()->dxy(PV.position())) < 0.045 &&
+          muon1.isPFMuon() &&
+          muon1.isGlobalMuon() &&
+          muon1.isTrackerMuon() &&
+          muon2.pt() > 15 &&
+          fabs(muon2.eta()) < 2.4 &&
+          //(muon2.chargedHadronIso() + muon2.neutralHadronIso() + muon2.photonIso())/muon2.pt() - 0.5 * (*rho) < 0.3 &&
+          (muon2.chargedHadronIso() + muon2.neutralHadronIso() + muon2.photonIso())/muon2.pt() < 0.3 &&
+          muon2.muonBestTrack()->dz(PV.position()) < 0.2 &&
+          abs(muon2.muonBestTrack()->dxy(PV.position())) < 0.045 &&
+          muon2.isPFMuon() &&
+          muon2.isGlobalMuon() &&
+          muon2.isTrackerMuon() &&
+          muon1.charge() * muon2.charge() < 0)
+        diMuon = true;
+    }
+  }  
+
+  // tau cands from jets and btag veto
+  bool atLeastOneBTag = false;
+  vector<const pat::Jet *> tauJetCands;
+  vector<int> tauJetCandsCharge;
+  for (const pat::Jet &jet : *jets) {
+    if (jet.pt() > 20 && fabs(jet.eta()) < 2.4 && jet.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags") > 0.890)
+      atLeastOneBTag = true;
+    bool noNearbyGlobalMuon = true;
+    for (const pat::Muon &muon : *muons) {
+      if (muon.pt() < 5) continue; 
+      if (!muon.isGlobalMuon()) continue;
+      double deltaR = reco::deltaR(muon.eta(),muon.phi(),jet.eta(),jet.phi());
+      if (deltaR < 0.4) noNearbyGlobalMuon = false;
+    }
+    if (jet.pt() > 20.0 &&
+        fabs(jet.eta()) < 2.3 &&
+        noNearbyGlobalMuon) {
+      double leading_track_pt = 0;
+      int leading_track_charge = 0;
+      for (unsigned int i = 0; i < jet.numberOfDaughters(); i++) {
+        if (jet.daughter(i)->pdgId() == 22 || jet.daughter(i)->pdgId() == 111) continue;
+        if (jet.daughter(i)->pt() > leading_track_pt) {
+          leading_track_pt = jet.daughter(i)->pt();
+          leading_track_charge = jet.daughter(i)->charge();
+        }
+      }
+      if (leading_track_pt > 5.0)
+        tauJetCands.push_back(&jet);
+        tauJetCandsCharge.push_back(leading_track_charge);
+    }
+  }
+
+  // tau cands from tau collection
+  vector<const pat::Tau *> tauCands;
+  for (const pat::Tau &tau : *taus) {
+    bool noNearbyGlobalMuon = true;
+    for (const pat::Muon &muon : *muons) {
+      if (muon.pt() < 5) continue; 
+      if (!muon.isGlobalMuon()) continue;
+      double deltaR = reco::deltaR(muon.eta(),muon.phi(),tau.eta(),tau.phi());
+      if (deltaR < 0.4) noNearbyGlobalMuon = false;
+    }
+    double leading_track_pt = (tau.leadChargedHadrCand())->pt();
+
+    if (tau.pt() > 20.0 &&
+        fabs(tau.eta()) < 2.3 &&
+        noNearbyGlobalMuon &&
+        tau.tauID("againstMuonTight3") == 1 &&
+        tau.tauID("againstElectronVLooseMVA6") == 1 &&
+        leading_track_pt > 5.0)
+          tauCands.push_back(&tau);
+  }
+
+  // choose single taujet-muon system
+  int muon_jet_index = -1;
+  int tau_jet_index = -1;
+  for (unsigned int i = 0; i < passedMuons.size(); i++) {
+    for (unsigned int j = 0; j < tauJetCands.size(); j++) {
+      const pat::Muon & muon = *passedMuons[i];
+      const pat::Jet & tau = *tauJetCands[j];
+      int tau_charge = tauJetCandsCharge[j];
+      double dR = reco::deltaR(muon.eta(),muon.phi(),tau.eta(),tau.phi());
+      int charge = muon.charge() * tau_charge;
+      if (dR < 0.5) continue;
+      if (charge > 0) continue;
+      if (muon_jet_index == -1) {
+        muon_jet_index = i;
+        tau_jet_index = j; }
+      else if (passedMuons[i]->pt() + tauJetCands[j]->pt() > passedMuons[muon_jet_index]->pt() + tauJetCands[tau_jet_index]->pt()) {
+        muon_jet_index = i;
+        tau_jet_index = j; }
+    }
+  }
+
+  // choose single tau-muon system
+  int muon_index = -1;
+  int tau_index = -1;
+  for (unsigned int i = 0; i < passedMuons.size(); i++) {
+    for (unsigned int j = 0; j < tauCands.size(); j++) {
+      const pat::Muon & muon = *passedMuons[i];
+      const pat::Tau & tau = *tauCands[j];
+      int tau_charge = tau.charge();
+      double dR = reco::deltaR(muon.eta(),muon.phi(),tau.eta(),tau.phi());
+      int charge = muon.charge() * tau_charge;
+      if (dR < 0.5) continue;
+      if (charge > 0) continue;
+      if (muon_index == -1) {
+        muon_index = i;
+        tau_index = j; }
+      else if (passedMuons[i]->pt() + tauCands[j]->pt() > passedMuons[muon_index]->pt() + tauCands[tau_index]->pt()) {
+        muon_index = i;
+        tau_index = j; }
+    }
+  }
+  
+  // filter decision
+  bool passTauMuonPair = false;
+  bool passDR = false;
+  bool passMT = false;
+  bool passPzeta = false;
+  bool passExtraLep = false;
+  bool passBTag = false;
+
+  if (cfg_tauObjs) {  
+    // at least one muon and one tau cand
+    if (muon_index != -1 && tau_index != -1) passTauMuonPair = true;
+
+    // tau-muon system: dR, opp sign, MT, Pzeta
+    if (passTauMuonPair) {
+      const pat::Muon & theMuon = *passedMuons[muon_index];
+      const pat::Tau & theTau = *tauCands[tau_index];
+
+      double dPhi = theMuon.phi() - MET.phi();
+      double MT = sqrt(2 * theMuon.pt() * MET.pt() * (1 - cos(dPhi)));    
+      TVector2 pTmuon;
+      pTmuon.SetMagPhi(theMuon.pt(), theMuon.phi());  
+      TVector2 pTtau;
+      pTtau.SetMagPhi(theTau.pt(), theTau.phi());  
+      TVector2 pTmet;
+      pTmet.SetMagPhi(MET.pt(), MET.phi());  
+      TVector2 zeta;
+      zeta.SetMagPhi(1.0, (theMuon.phi()-theTau.phi())/2.0 + theTau.phi());
+      double PzetaAll = zeta * (pTmuon + pTtau + pTmet);
+      double PzetaVis = zeta * (pTmuon + pTtau);
+      double Pzeta = PzetaAll - 0.85 * PzetaVis;
+
+      if (reco::deltaR(theMuon.eta(), theMuon.phi(), theTau.eta(), theTau.phi()) > 0.5) passDR = true;
+      if (MT < 40) passMT = true;
+      if (Pzeta > -25) passPzeta = true;
+    }
+  } else {
+    // at least one muon and one tau cand
+    if (muon_jet_index != -1 && tau_jet_index != -1) passTauMuonPair = true;
+
+    // tau-muon system: dR, opp sign, MT, Pzeta
+    if (passTauMuonPair) {
+      const pat::Muon & theMuon = *passedMuons[muon_jet_index];
+      const pat::Jet & theTau = *tauJetCands[tau_jet_index];
+
+      double dPhi = theMuon.phi() - MET.phi();
+      double MT = sqrt(2 * theMuon.pt() * MET.pt() * (1 - cos(dPhi)));    
+      TVector2 pTmuon;
+      pTmuon.SetMagPhi(theMuon.pt(), theMuon.phi());  
+      TVector2 pTtau;
+      pTtau.SetMagPhi(theTau.pt(), theTau.phi());  
+      TVector2 pTmet;
+      pTmet.SetMagPhi(MET.pt(), MET.phi());  
+      TVector2 zeta;
+      zeta.SetMagPhi(1.0, (theMuon.phi()-theTau.phi())/2.0 + theTau.phi());
+      double PzetaAll = zeta * (pTmuon + pTtau + pTmet);
+      double PzetaVis = zeta * (pTmuon + pTtau);
+      double Pzeta = PzetaAll - 0.85 * PzetaVis;
+
+      if (reco::deltaR(theMuon.eta(), theMuon.phi(), theTau.eta(), theTau.phi()) > 0.5) passDR = true;
+      if (MT < 40) passMT = true;
+      if (Pzeta > -25) passPzeta = true;
+    }
+  }
+
+  // extra lepton veto
+  if (!extraMuon && !extraElectron && !diMuon) passExtraLep = true;
+
+  // btag veto
+  if (!atLeastOneBTag) passBTag = true;
+
+  // final filter decision
+  bool passAll = passMuonTrigger && passTauMuonPair && passDR && passMT && passPzeta && passExtraLep && passBTag;
+
+  // cutflow branches
+  bpassMuonTrigger = passMuonTrigger;
+  bpassMuon = (passedMuons.size() > 0);
+  if (!cfg_tauObjs)  bpassTauCand = (tauJetCands.size() > 0);
+  else bpassTauCand = (tauCands.size() > 0);
+  bpassTauMuonPair = passTauMuonPair;
+  bpassDR = passDR;
+  bpassMT = passMT;
+  bpassPzeta = passPzeta;
+  bpassExtraLep = passExtraLep;
+  bpassBTag = passBTag;
+  bpassAll = passAll;
+
+  // fill tree
+  tree_->Fill();
+}
+
+void
+ZtoTauHadCutflowMaker::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.addUntracked<bool>("tauObjs", false);
+  descriptions.add("ZtoTauHadCutflowMakerFilter", desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(ZtoTauHadCutflowMaker);
--- TauHadFilters/test/filter_tree_cfg.py
+++ TauHadFilters/test/filter_tree_cfg.py
@@ -5,18 +5,23 @@ process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100000) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
-  #'file:/cms/chiarito/samples/ztoll/miniaod/merged10k_30prong_pt40_eta25_MINIAOD.root'
   #'/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/00271851-3CC8-E611-AFB9-002590D0AFD8.root' # 10-50
   '/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v1/100000/00099D43-77ED-E611-8889-5065F381E1A1.root' # 50+
+  #'root://cms-xrd-global.cern.ch//store/data/Run2016C/SingleMuon/MINIAOD/07Aug17-v1/110000/0028E5A8-4E80-E711-A781-001E677926A2.root' # SingleMuon data
   ))
-process.TFileService = cms.Service("TFileService", fileName = cms.string( "output_tree_tauobjs.root" ) )
 
-process.tree = cms.EDAnalyzer('ZtoTauHadCutflowMaker',
+process.filt = cms.EDFilter('ZtoTauHadRecoSelector',
+  )
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string( "tree.root" ) )
+
+process.tree = cms.EDAnalyzer('ZtoTauHadTreeMaker',
+  recoOnly = cms.untracked.bool(False),
   tauObjs = cms.untracked.bool(False)
   )
 
-process.p = cms.Path(process.tree)
+process.p = cms.Path(process.filt * process.tree)
